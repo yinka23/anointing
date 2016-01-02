d39d523e081d00b88ab5bf9cab3b4f431991309e
Yinka Salam <psoft.yinka@.com>
2016-01-02 14:56:02 -0600

    phase one

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
+	class Contact < ActiveRecord::Base
+        validates :name, presence: true
+  validates :email, presence: true
+end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 root 'pages#home'
 get '/about' => 'pages#about'
-get '/about' => 'pages#service'
+get '/service' => 'pages#service'
+resources :contacts
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
